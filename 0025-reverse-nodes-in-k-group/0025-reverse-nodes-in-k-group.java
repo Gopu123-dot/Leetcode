df58d61ea6560b6fa4fddbfad82daa83e23eb8cc
GOPU SRI <g.gopusri@gmail.com>
2026-03-24 19:22:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if(head==null || k==1)
+        if(head==null || head.next==null || k==1)
         return head;
         ListNode dummy=new ListNode(0);
         dummy.next=head;
