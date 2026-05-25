724dc3a72a18d81c6bf88aaa09cbb7030bcbf5c9
GOPU SRI <g.gopusri@gmail.com>
2026-05-25 20:52:28 +0530

    Time: 11 ms (83.93%), Space: 47.1 MB (44.94%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue <Integer> radiant=new LinkedList<>();
+        Queue <Integer> dire=new LinkedList<>();
+        int n=senate.length();
+        for(int i=0;i<n;i++) {
+            if(senate.charAt(i)=='R')
+            radiant.offer(i);
+            else
+            dire.offer(i);
+        }
+        while(!radiant.isEmpty() && !dire.isEmpty()) {
+            int a=radiant.poll();
+            int b=dire.poll();
+            if(a<b) {
+                radiant.offer(a+n);
+            }
+            else {
+                dire.offer(b+n);
+            }
+        }
+        return radiant.isEmpty() ? "Dire" : "Radiant";
+    }
+}
