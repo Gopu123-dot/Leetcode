d535d1433a0e6490754be97c9a2a7b4b30b5b882
GOPU SRI <g.gopusri@gmail.com>
2026-05-25 20:47:23 +0530

    Time: 23 ms (30.21%), Space: 59.7 MB (23.04%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -3,10 +3,9 @@ class RecentCounter {
     public RecentCounter() {
         q=new LinkedList<>();
     }
-    
     public int ping(int t) {
         q.offer(t);
-        while(q.peek()<t-3000) {
+        while(!q.isEmpty() && q.peek()<t-3000) {
             q.poll();
         }
         return q.size();
