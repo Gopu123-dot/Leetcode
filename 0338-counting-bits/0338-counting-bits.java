ba9bef9a282ba90d828614e415513074d7f004c1
GOPU SRI <g.gopusri@gmail.com>
2026-06-08 19:52:56 +0530

    Time: 2 ms (96.74%), Space: 48.6 MB (67.14%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,13 +2,7 @@ class Solution {
     public int[] countBits(int n) {
         int []dp=new int[n+1];
         for(int i=0;i<=n;i++) {
-            int num=i;
-            int c=0;
-            while(num>0) {
-                c+=num%2;
-                num/=2;
-            }
-            dp[i]=c;
+            dp[i]=dp[i>>1]+(i&1);
         }
         return dp;
     }
