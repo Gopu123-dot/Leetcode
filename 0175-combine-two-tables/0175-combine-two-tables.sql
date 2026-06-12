ec2f7670429144f98e62def2183c54cb9c8ebc9b
GOPU SRI <g.gopusri@gmail.com>
2026-06-12 17:18:44 +0530

    Time: 513 ms (9.41%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
 
-select e.firstName,e.lastName,a.city,a.state 
-from person e
+select
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state 
+from person p
 left join address a
-on e.personid=a.personid
-order by e.firstname;
+on p.personid=a.personid
+order by p.firstname;
