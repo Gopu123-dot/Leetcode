72c8cf6d83d8496697a5c0837ab7b1b515c51c33
GOPU SRI <g.gopusri@gmail.com>
2026-06-30 21:30:20 +0530

    Time: 1 ms (99.71%), Space: 47.6 MB (8.49%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump=0,max=0,end=0;
+        for(int i=0;i<nums.length-1;i++) {
+            max=Math.max(max,i+nums[i]);
+            if(i==end) {
+                jump++;
+                end=max;
+            }
+        }
+        return jump;
+    }
+}
