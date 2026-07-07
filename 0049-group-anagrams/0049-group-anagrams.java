4674ff457305bf9b5cae5cbf4c5d8f513696a686
GOPU SRI <g.gopusri@gmail.com>
2026-07-07 22:07:56 +0530

    Time: 7 ms (73.66%), Space: 49.5 MB (84.15%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>h=new HashMap<>();
+        for(String s:strs) {
+            char []c=s.toCharArray();
+            Arrays.sort(c);
+            String key=new String(c);
+            while(!h.containsKey(key)) {
+                h.put(key,new ArrayList<>());
+            }
+            h.get(key).add(s);
+        }
+        return new ArrayList<>(h.values());
+    }
+}
